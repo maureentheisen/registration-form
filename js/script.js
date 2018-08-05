31190b35fe43b334a8a1e9f32873163bf4dba132
maureen <maureenrtheisen@gmail.com>
2018-08-05 15:32:32 -0400

    working on making sure only appropriate Tshirt color options appear based on Design choice

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const selectJob = document.getElementById('title');
-
   const otherJobTitle = document.getElementById('other-title');
+  const tshirtDesign = document.getElementById('design');
+  const tshirtColor = document.getElementById('color');
+
 
   otherJobTitle.style.display = 'none';
 
-title.addEventListener('change', (e) => {
-  if(selectJob.options[selectJob.selectedIndex].value === 'other'){
-      otherJobTitle.style.display = 'block';
-  };
-})
+  // show the your job role text input box on changing selection value
+  title.addEventListener('change', (e) => {
+    if(selectJob.options[selectJob.selectedIndex].value === 'other'){
+        otherJobTitle.style.display = 'block';
+    };
+  })
+
+  tshirtDesign.addEventListener('change', (event) => {
+    if(event.target.value === 'js puns'){
+      for(let i=0; i<tshirtColor.options.length; i++){
+        if(
+          (tshirtColor.options[i].value === 'tomato') ||
+          (tshirtColor.options[i].value === 'steelblue') ||
+          (tshirtColor.options[i].value === 'dimgrey')
+        ){
+          tshirtColor.options[i].style.display = 'none';
+        }else{
+          tshirtColor.options[i].style.display = 'block';
+        }
+      }
+    } else if(event.target.value === 'heart js'){
+      for(var i=0; i<tshirtColor.options.length; i++){
+        if(
+          (tshirtColor.options[i].value === 'cornflowerblue') ||
+          (tshirtColor.options[i].value === 'darkslategrey') ||
+          (tshirtColor.options[i].value === 'gold')
+        ){
+          tshirtColor.options[i].style.display = 'none';
+        }else{
+          tshirtColor.options[i].style.display = 'block';
+        }
+      }
+    };
+  });
+
+
+
 
 
 
